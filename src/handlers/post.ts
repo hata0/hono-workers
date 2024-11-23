2b0be7beb5ee1469a310c72abf9ee305f6d9fd31
hata0 <haataa0100@gmail.com>
2024-11-23 14:40:31 +0900

    feat: prismaを使ってデータ取得できるように実装

--- src/handlers/post.ts
+++ src/handlers/post.ts
@@ -1,4 +1,7 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 import type { Context } from "hono";
+import { Pool } from "pg";
 
 export class PostHandler {
   get(c: Context) {
@@ -11,16 +14,13 @@ export class PostHandler {
     return c.json(result, 200);
   }
 
-  list(c: Context) {
-    const result = {
-      posts: [
-        {
-          name: "foo",
-          title: "Sample post 1",
-          body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum feugiat nunc, id elementum nulla venenatis ut. Praesent non nunc ultrices, consequat arcu sit amet, auctor lacus. Maecenas libero sem, tincidunt ut sapien quis, elementum vehicula augue. Cras non cursus nulla. Suspendisse congue posuere accumsan. Vivamus ut sollicitudin ligula. In viverra tellus vel porttitor feugiat. Ut bibendum turpis sed mauris egestas ultricies.",
-        },
-      ],
-    };
+  async list(c: Context) {
+    const connectionString = `${c.env?.DATABASE_URL ?? ""}`;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    const prisma = new PrismaClient({ adapter });
+
+    const result = await prisma.post.findMany();
 
     return c.json(result, 200);
   }
